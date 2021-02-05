d9845bb1849579f587cf7357f83fd250e28801fb
Nihad <54079962+nyhead@users.noreply.github.com>
2021-02-05 15:42:37 +0400

    Update model.py

--- model.py
+++ model.py
@@ -2,4 +2,7 @@ import random
 
 class Game:
     def __init__(self):
-        self.a = [random.randint(1,10) for i in range(10)]
+        # self.a = [random.randint(1,10) for i in range(10)]
+        self.a = [6,1,4,3,2,5,8,7]
+        self.arr_elem = []
+        self.FPS = 25
